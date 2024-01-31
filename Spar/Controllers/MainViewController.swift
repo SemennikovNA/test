a2b2914ab164331ad03152a2f91c79b0ce053db1
Nikita <semennikovna@yandex.ru>
2024-01-31 13:20:22 +0600

    Inital commit

---
+++ Spar/Controllers/MainViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Spar
+//
+//  Created by Nikita on 31.01.2024.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    
+    //MARK: - Life cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+
+}
+
