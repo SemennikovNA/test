8de61f04d4347e8363caf954aa63939b7afda447
Nikita <semennikovna@yandex.ru>
2024-01-31 19:46:53 +0600

    Added scroll view

--- Spar/Controllers/MainViewController.swift
+++ Spar/Controllers/MainViewController.swift
@@ -16,6 +16,13 @@ final class MainViewController: UIViewController {
     //MARK: - User elements
     
     private lazy var priceView = PriceView()
+    private lazy var scrollView: UIScrollView = {
+        let scroll = UIScrollView()
+        scroll.alwaysBounceVertical = true
+        scroll.showsVerticalScrollIndicator = false
+        return scroll
+    }()
+    private lazy var contentView = UIView()
     
     //MARK: - Life cycle
     
@@ -32,7 +39,9 @@ final class MainViewController: UIViewController {
     private func setupView() {
         // Setup view
         view.backgroundColor = .white
-        view.addSubviews(priceView)
+        contentView.backgroundColor = .black
+        view.addSubviews(scrollView, priceView)
+        scrollView.addSubviews(contentView)
         
         // Setup custom view
         priceView.dropShadow()
@@ -56,16 +65,11 @@ final class MainViewController: UIViewController {
     @objc private func minusButtonTapped() {
         if values.countOfProduct > 1 {
               values.countOfProduct -= 1
-              
               if values.countOfProduct == 0 {
                   values.countOfProduct = 1
               }
-              
               let price = String(values.productPrice / values.countOfProduct)
-              
-              
               let title = String(values.countOfProduct)
-              
               priceView.greenPriceView.setTitleForProductCountLabel(title: title)
               priceView.greenPriceView.setTitleForPriceLabel(title: price)
           }
@@ -76,7 +80,6 @@ final class MainViewController: UIViewController {
         values.countOfProduct += 1
         let price = String(values.productPrice * values.countOfProduct)
         let title = String(values.countOfProduct)
-        
         priceView.greenPriceView.setTitleForProductCountLabel(title: title)
         priceView.greenPriceView.setTitleForPriceLabel(title: price)
     }
@@ -87,8 +90,20 @@ final class MainViewController: UIViewController {
 private extension MainViewController {
     /// Method for setup contraints main view controller
     func setupConstraints() {
-        
         NSLayoutConstraint.activate([
+            // Scroll view
+            scrollView.topAnchor.constraint(equalTo: view.topAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: priceView.topAnchor),
+            
+            // Content view
+            contentView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            contentView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            contentView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+            contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            
+            // Price view
             priceView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             priceView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             priceView.heightAnchor.constraint(equalToConstant: 140),
